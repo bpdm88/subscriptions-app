16acbaeb835dbbf716116128306c5a883a7d5fdc
Ben <benjamin.main88@gmail.com>
2020-12-15 12:44:57 +0000

    function can now determine how many days difference between current date and date given by API

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -18,6 +18,7 @@ class List extends Component {
 
     render() {
         const { list } = this.props;
+
         return (
             <ul>
                 {list.map((item) => (
@@ -26,11 +27,35 @@ class List extends Component {
                         onClick={this.handleClick}
                         data-id={item.id}
                     >
+                        {console.log(item.payment_date)}
+                        {console.log(whatColour(item.payment_date))}
+                        {console.log(new Date())}
+
                         {item.subscription_name}
-                        <TrafficLight colourCode={2} />
+
+                        <TrafficLight colourCode={1} />
                     </li>
                 ))}
             </ul>
         );
     }
 }
+
+export default List;
+
+let whatColour = (payment) => {
+    let dateString = payment;
+    let year = dateString.substring(0, 4);
+    let month = dateString.substring(4, 6);
+    let day = dateString.substring(6, 8);
+
+    let date = new Date(year, month - 1, day);
+
+    let current = new Date();
+
+    let difference = date - current; // milliseconds difference
+
+    let days = difference / (1000 * 60 * 60 * 24); // turns milliseconds to days
+
+    return days.toFixed(1);
+};
