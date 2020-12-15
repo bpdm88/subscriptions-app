aad256e7f2827925f22d4bdde4020892030b84e2
Ben <benjamin.main88@gmail.com>
2020-12-15 15:21:38 +0000

    moved colour logic in to class

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,6 +6,7 @@ class List extends Component {
         super();
 
         this.handleClick = this.handleClick.bind(this);
+        this.whatColour = this.whatColour.bind(this);
     }
 
     componentDidMount() {
@@ -14,6 +15,29 @@ class List extends Component {
 
     handleClick(event) {
         this.props.setSelected(event.currentTarget.dataset.id);
+    }
+
+    whatColour(payment) {
+        let dateString = payment;
+        let year = dateString.substring(0, 4);
+        let month = dateString.substring(4, 6);
+        let day = dateString.substring(6, 8);
+
+        let date = new Date(year, month - 1, day); // creates payment date based off string
+
+        let current = new Date(); // the current date
+
+        let difference = date - current; // milliseconds difference
+
+        let days = difference / (1000 * 60 * 60 * 24); // turns milliseconds to days
+
+        let rounded = days.toFixed(1); // rounded day
+
+        if (rounded <= 5) {
+            return 3;
+        } else if (rounded <= 10) {
+            return 2;
+        } else return 1;
     }
 
     render() {
@@ -30,7 +54,7 @@ class List extends Component {
                         {item.subscription_name}
 
                         <TrafficLight
-                            colourCode={whatColour(item.payment_date)}
+                            colourCode={this.whatColour(item.payment_date)}
                         />
                     </li>
                 ))}
@@ -40,27 +64,3 @@ class List extends Component {
 }
 
 export default List;
-
-let whatColour = (payment) => {
-    let dateString = payment;
-
-    let year = dateString.substring(0, 4);
-    let month = dateString.substring(4, 6);
-    let day = dateString.substring(6, 8);
-
-    let date = new Date(year, month - 1, day); // creates payment date based off string
-
-    let current = new Date(); // the current date
-
-    let difference = date - current; // milliseconds difference
-
-    let days = difference / (1000 * 60 * 60 * 24); // turns milliseconds to days
-
-    let rounded = days.toFixed(1); // rounded day
-
-    if (rounded <= 5) {
-        return 3;
-    } else if (rounded <= 10) {
-        return 2;
-    } else return 1;
-};
