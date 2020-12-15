a0a1f3471a89ccf1f052a8a15e8956879385f932
Jacques <jacquescoetzee91@gmail.com>
2020-12-15 11:49:54 +0000

    updated subscription wrapper to remove unused reduce and added export statemetn to List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -34,3 +34,6 @@ class List extends Component {
         );
     }
 }
+
+export default List;
+
--- src/components/Subscription/index.js
+++ src/components/Subscription/index.js
@@ -5,14 +5,7 @@ import { connect } from "react-redux";
 const mapStateToProps = (state) => {
     let current = state.subscriptions.filter( item => item.id === state.selected );
 
-    let reduced = state.subscriptions.reduce(( obj, item ) => {
-        if(item.id === state.selected){
-            return item;
-        }
-    },{});
-
     return {
-        data: state.subscriptions,
         selected: current,
     };
 };
