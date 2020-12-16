3267a574f7aad9a3ff5e5359223f8dd53ef07d2f
Jacques <jacquescoetzee91@gmail.com>
2020-12-16 14:42:30 +0000

    updating text in subscription p tag

--- src/components/Subscription/Subscription.js
+++ src/components/Subscription/Subscription.js
@@ -8,11 +8,7 @@ const Subscription = ({ selected, listID }) => {
         <>
             <p>Monthly Cost £{selected.cost}</p>
             <p>
-                Avoid next payment ? Cancel in 
-                {
-                    daysToCancel(selected.notice_period, selected.payment_date)
-                } 
-                days
+                Avoid next payment ? Cancel in { daysToCancel(selected.notice_period, selected.payment_date) } days
             </p>
             <p>Spent so far £{amountPaid(selected.start, +selected.cost)}</p>
             <button className="delete">x</button>
