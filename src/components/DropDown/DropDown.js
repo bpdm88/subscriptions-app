097ac6c188c63817637b62d6d3e27db3901e33b6
Jacques <jacquescoetzee91@gmail.com>
2020-12-17 09:22:04 +0000

    made dropdown into class based with handleClick

--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -1,9 +1,30 @@
-import Dropdown from 'react-bootstrap/Dropdown';
+import { Component } from "react";
 
-const DropDown = () => {
-    return(
+class DropDown extends Component {
+    constructor(props) {
+        super(props);
 
-    );
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        event.preventDefault();
+        console.log(event.currentTarget.innerText);
+    }
+
+    render() {
+        return(
+            <div class="dropdown">
+                <button class="dropbtn">Catagories</button>
+                <div class="dropdown-content">
+                    <button onClick={ this.handleClick }>Link 1</button>
+                    <button onClick={ this.handleClick }>Link 2</button>
+                    <button onClick={ this.handleClick }>Link 3</button>
+                </div>
+            </div>
+        );
+    }
 }
+
 
 export default DropDown;
