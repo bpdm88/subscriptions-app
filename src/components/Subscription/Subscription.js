84e695d2124e8b38261e04cc07cdfe85087c5cd7
Tumeka Burston <burston1993@icloud.com>
2020-12-17 11:33:06 +0000

    updated styling for form button and sorted out card header to have better ease on eyes

--- src/components/Subscription/Subscription.js
+++ src/components/Subscription/Subscription.js
@@ -6,14 +6,14 @@ const Subscription = ({ selected, listID }) => {
     let display = selected && selected.id === listID;
     return (
         !display ? null :
-        <>
+        <div className="card-items">
             <p>Monthly Cost: £{selected.cost}</p>
-            <p>Next Payment Date: ___</p>
+            <p>Next Payment Date: </p>
             <p>Spent so far: £{amountPaid(selected.start, +selected.cost)}</p>
-            <hr/>
+            
             <p>Cancel in: { daysToCancel(selected.notice_period, selected.payment_date) } days</p>
             <Button className="delete" itemID={ listID }></Button>
-        </>
+        </div>
     );
 };
 
